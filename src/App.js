10d848ba940e3d13a3fbd0f64d6ac9ac62709b67
Esha98 <eshashah3335@gmail.com>
2022-06-20 07:34:21 +1000

    :sparkles: added box with rotation and starts as a background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function App() {
-  return;
+import { Canvas, useFrame } from "react-three-fiber";
+import { Stars, OrbitControls } from "@react-three/drei";
+
+import "./style.css";
+
+function Box() {
+  const mesh = useRef();
+  useFrame(() => {
+    mesh.current.rotation.y = mesh.current.rotation.x += 0.01;
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <OrbitControls />
+      <Stars />
+      <Box />
+    </Canvas>
+  );
+}
